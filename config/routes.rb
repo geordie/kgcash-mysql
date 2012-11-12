ba849c337ebb8f3b525da9f08fb5cd0d7624092a
Geordie Henderson <geordie@waterbay.com>
2012-11-12 18:51:17 +0100

    Implemented authentication using Sorcery based on this tutorial: https://github.com/NoamB/sorcery/wiki/Simple-Password-Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Kgcash::Application.routes.draw do
+
+root :to => 'users#index'
+resources :user_sessions
+resources :users
+  
+match 'login' => 'user_sessions#new', :as => :login
+match 'logout' => 'user_sessions#destroy', :as => :logout
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
