352f12e53607d5f32787c19f01a1db0927322768
Geordie Henderson <geordie@waterbay.com>
2012-11-18 05:12:39 +0100

    Added budget categories to seed DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,29 @@ categories = Category.create(
   { name: 'Not defined' },
   { name: 'Beer and Wine'}])
 
-budgetcategories = BudgetCategory.create([{amount:800, period:'MONTHLY', budget_id: budgets[0], category_id: categories[1] }])
+budgetcategories = BudgetCategory.create([
+ {amount:2800, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[0].id },
+ {amount:850, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[1].id },
+ {amount:350, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[2].id },
+ {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[3].id },
+ {amount:100, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[4].id },
+ {amount:850, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[5].id },
+ {amount:250, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[6].id },
+ {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[7].id },
+ {amount:400, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[8].id },
+ {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[9].id },
+ {amount:500, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[10].id },
+ {amount:400, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[11].id },
+ {amount:400, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[12].id },
+ {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[13].id },
+ {amount:275, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[14].id },
+ {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[15].id },
+ {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[16].id },
+ {amount:175, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[17].id },
+ {amount:150, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[18].id }
+  ])
+
+puts 'DONE SEED'
+
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
