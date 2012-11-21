30e9ac22937991f655bc3e6318d2362359cbcda4
Geordie Henderson <geordie@waterbay.com>
2012-11-21 08:19:23 +0100

    Added testing gem and basic unit testing for category

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -4,4 +4,28 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "category will not save without a name" do
+    category = Category.new
+    assert !category.save 
+  end
+
+  test "category will not save without name longer than 3 characers" do
+    category = Category.new
+    category.name = "ab"
+    assert !category.save
+  end
+
+  test "category will save with valid name" do
+    category = Category.new
+    category.name = "Groceries"
+    assert category.save
+  end
+
+  test "category will save with name and desc" do
+    category = Category.new
+    category.name = "Salary"
+    category.description = "Money earned"
+    assert category.save
+  end
 end
