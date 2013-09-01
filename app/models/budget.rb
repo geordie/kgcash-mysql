b840a541743c43f5d4392497a872aaae771f41f7
Geordie Henderson <geordie@waterbay.com>
2013-09-01 07:03:55 +0200

    Sort categories in transactions view dropdown using a model method

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,4 +2,8 @@ class Budget < ActiveRecord::Base
   belongs_to :user
   has_many :budget_categories, :dependent => :delete_all
   has_many :categories, :through => :budget_categories
+
+  def sortedCategories
+  	return self.categories.sort!{|a,b| a.name.downcase <=> b.name.downcase }
+  end
 end
