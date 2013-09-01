dbdfceda9d11a8fb3c5db963ea2d6d3aa62a4fbd
Geordie Henderson <geordie@waterbay.com>
2013-09-01 06:40:07 +0200

    Improved budget category display with colour classes for amounts

--- app/models/budget_category.rb
+++ app/models/budget_category.rb
@@ -1,4 +1,11 @@
 class BudgetCategory < ActiveRecord::Base
   belongs_to :budget
   belongs_to :category
+
+  def isDebit
+  	if self.category.cat_type == "Income"
+  		return false
+  	end
+  	return true
+  end
 end
