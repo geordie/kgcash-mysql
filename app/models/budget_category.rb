a4280be6815baef055dcb0f5e836fd9887c0c10d
Geordie Henderson <geordie@waterbay.com>
2013-09-11 04:56:42 +0200

    Resolved issue with budget_category.isDebit method that caused problems when cat_type is null

--- app/models/budget_category.rb
+++ app/models/budget_category.rb
@@ -3,7 +3,9 @@ class BudgetCategory < ActiveRecord::Base
   belongs_to :category
 
   def isDebit
-  	if self.category.cat_type == "Income"
+  	if self.category.is_nil?
+  		return true
+  	elsif self.category.cat_type == "Income"
   		return false
   	end
   	return true
