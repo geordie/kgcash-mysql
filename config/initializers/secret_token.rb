8a91b5d8dda732a02757da072d387da04ab3a657
geordie <geordie.a.henderson@gmail.com>
2015-11-08 23:18:33 -0800

    Derive secret token from environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kgcash::Application.config.secret_token = [RAILS_GENERATED_SECRET_TOKEN]
+Kgcash::Application.config.secret_token = ENV['KGCASH_SECRET_TOKEN']
