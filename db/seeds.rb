f98d581904050ffec1c6b2ab27d77b7a1a16603b
vagrant <vagrant@precise32.(none)>
2016-10-24 07:18:34 +0000

    First step in creating a sample user in seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,68 +5,52 @@
 #
 
 users = User.create([
-    {username: 'geordie', email: 'geordie.a.henderson@gmail.com', crypted_password: "$2a$10$Ub1mJwax0nZpN5JU9ED5eu4EE.ad1.95K1dXi.J8DAdLBHPDn6oi6", salt: 'VTKmPVJFsaivMbFxVv5F'}
+    {username: 'example', email: 'geordie.a.henderson@gmail.com', crypted_password: "$2a$10$Ub1mJwax0nZpN5JU9ED5eu4EE.ad1.95K1dXi.J8DAdLBHPDn6oi6", salt: 'VTKmPVJFsaivMbFxVv5F'}
   ])
 
-budgets = Budget.create([{name: 'joint', description: 'kt and geordie joint', user_id: users[0]}])
+budgets = Budget.create([{name: 'Example Budget', description: 'A basic example budget', user_id: users[0]}])
 
-categories = Category.create(
-  [{ name: 'Childcare', user_id: users[0] },
-  { name: 'Groceries', user_id: users[0] },
-  { name: 'Utilities', user_id: users[0] },
-  { name: 'Gas', user_id: users[0] },
-  { name: 'Bucc Bay 57', user_id: users[0] },
-  { name: 'House', user_id: users[0] },
-  { name: 'Medical', user_id: users[0] },
-  { name: 'Dining', user_id: users[0] },
-  { name: 'Katie', user_id: users[0] },
-  { name: 'Clothing', user_id: users[0] },
-  { name: 'Savings', user_id: users[0] },
-  { name: 'Bucc Bay 58', user_id: users[0] },
-  { name: 'Geordie', user_id: users[0] },
-  { name: 'Car', user_id: users[0] },
-  { name: 'House Cleaning', user_id: users[0] },
-  { name: 'Travel', user_id: users[0] },
-  { name: 'Kids', user_id: users[0] },
-  { name: 'Royal Van', user_id: users[0] },
-  { name: 'Gifts', user_id: users[0] },
-  { name: 'RESP', user_id: users[0] },
-  { name: 'Tennis Club', user_id: users[0] },
-  { name: 'Mobile Phone', user_id: users[0] },
-  { name: 'Gifts', user_id: users[0] },
-  { name: 'Salary - Katie', user_id: users[0] },
-  { name: 'Salary - Geordie', user_id: users[0] },
-  { name: 'Salary - Kids', user_id: users[0] },
-  { name: 'Not defined', user_id: users[0] },
-  { name: 'Beer and Wine', user_id: users[0]}])
+categories = Category.create([
+{ name: 'Clothing', user_id: users[0] },
+{ name: 'Dining', user_id: users[0] },
+{ name: 'Discretionary', user_id: users[0] },
+{ name: 'Groceries', user_id: users[0] },
+{ name: 'Gifts', user_id: users[0] },
+{ name: 'Home', user_id: users[0] },
+{ name: 'Insurance', user_id: users[0] },
+{ name: 'Internet', user_id: users[0] },
+{ name: 'Medical', user_id: users[0] },
+{ name: 'Rent', user_id: users[0] },
+{ name: 'Phone', user_id: users[0] },
+{ name: 'Property Tax', user_id: users[0] },
+{ name: 'Savings', user_id: users[0] },
+{ name: 'Transportation', user_id: users[0] },
+{ name: 'Travel', user_id: users[0] },
+{ name: 'Utilities', user_id: users[0] }])
 
 budgetcategories = BudgetCategory.create([
- {amount:2800, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[0].id },
- {amount:850, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[1].id },
- {amount:350, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[2].id },
- {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[3].id },
+ {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[0].id },
+ {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[1].id },
+ {amount:250, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[2].id },
+ {amount:500, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[3].id },
  {amount:100, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[4].id },
- {amount:850, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[5].id },
- {amount:250, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[6].id },
- {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[7].id },
- {amount:400, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[8].id },
- {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[9].id },
- {amount:500, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[10].id },
- {amount:400, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[11].id },
- {amount:400, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[12].id },
+ {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[5].id },
+ {amount:100, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[6].id },
+ {amount:100, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[7].id },
+ {amount:1200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[8].id },
+ {amount:75, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[9].id },
+ {amount:100, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[10].id },
+ {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[11].id },
+ {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[12].id },
  {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[13].id },
- {amount:275, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[14].id },
- {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[15].id },
- {amount:200, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[16].id },
- {amount:175, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[17].id },
- {amount:150, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[18].id }
+ {amount:300, period:'MONTHLY', budget_id: budgets[0].id, category_id: categories[14].id }
   ])
 
-transactions = Transaction.create([
-  {tx_hash:'abc',   tx_date:DateTime.now, debit:100.23, category_id:categories[0], user_id:users[0]},
-  {tx_hash:'defg', tx_date:DateTime.now, debit: 56.78, category_id:categories[1], user_id:users[0]},
-  {tx_hash:'hij',  tx_date:DateTime.now, debit: 18.68, category_id:categories[2], user_id:users[0]}
-  ])
+#transactions = Transaction.create([
+#  {tx_hash:'abc',   tx_date:DateTime.now, debit:100.23, category_id:categories[0], user_id:users[0]},
+#  {tx_hash:'defg', tx_date:DateTime.now, debit: 56.78, category_id:categories[1], user_id:users[0]},
+#  {tx_hash:'hij',  tx_date:DateTime.now, debit: 18.68, category_id:categories[2], user_id:users[0]}
+#  ])
 
 puts 'DONE SEED'
 
