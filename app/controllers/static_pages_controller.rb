538b0b9e3c8a5e0335642352a03b90344a667315
geordie-henderson-hs <geordie.henderson@hootsuite.com>
2017-06-03 22:46:44 -0700

    Part way through static home page setup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
+  skip_before_filter :require_login
+  layout :false
+
   def home
+    respond_to do |format|
+			format.html
+		end
   end
 
 end
