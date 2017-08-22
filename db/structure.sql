5fc722653a26079a971c99aa58feb51d8aa3ddd4
Geordie Henderson <geordie.a.henderson@gmail.com>
2017-08-21 21:55:31 -0700

    Updated db structure file

--- db/structure.sql
+++ db/structure.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: kgcash_dev
--- ------------------------------------------------------
--- Server version	5.5.53-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,11 +9,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `accounts`
---
-
 DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -30,13 +20,8 @@ CREATE TABLE `accounts` (
   `updated_at` datetime DEFAULT NULL,
   `account_type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `accounts_users`
---
-
 DROP TABLE IF EXISTS `accounts_users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -45,13 +30,19 @@ CREATE TABLE `accounts_users` (
   `account_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `budget_categories`
---
-
+DROP TABLE IF EXISTS `ar_internal_metadata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar_internal_metadata` (
+  `key` varchar(255) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `budget_categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -66,13 +57,8 @@ CREATE TABLE `budget_categories` (
   PRIMARY KEY (`id`),
   KEY `index_budget_categories_on_budget_id` (`budget_id`),
   KEY `index_budget_categories_on_category_id` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=442 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `budgets`
---
-
 DROP TABLE IF EXISTS `budgets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -85,13 +71,8 @@ CREATE TABLE `budgets` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_budgets_on_user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `categories`
---
-
 DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -104,13 +85,8 @@ CREATE TABLE `categories` (
   `user_id` int(11) DEFAULT NULL,
   `cat_type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=372 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `oauth_access_grants`
---
-
 DROP TABLE IF EXISTS `oauth_access_grants`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -128,11 +104,6 @@ CREATE TABLE `oauth_access_grants` (
   UNIQUE KEY `index_oauth_access_grants_on_token` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `oauth_access_tokens`
---
-
 DROP TABLE IF EXISTS `oauth_access_tokens`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -152,11 +123,6 @@ CREATE TABLE `oauth_access_tokens` (
   KEY `index_oauth_access_tokens_on_resource_owner_id` (`resource_owner_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `oauth_applications`
---
-
 DROP TABLE IF EXISTS `oauth_applications`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -175,24 +141,14 @@ CREATE TABLE `oauth_applications` (
   KEY `index_oauth_applications_on_owner_id_and_owner_type` (`owner_id`,`owner_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `schema_migrations`
---
-
 DROP TABLE IF EXISTS `schema_migrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `schema_migrations` (
   `version` varchar(255) NOT NULL,
-  UNIQUE KEY `unique_schema_migrations` (`version`)
+  PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `transactions`
---
-
 DROP TABLE IF EXISTS `transactions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -215,13 +171,8 @@ CREATE TABLE `transactions` (
   KEY `index_transactions_on_category_id` (`category_id`),
   KEY `index_transactions_on_user_id` (`user_id`),
   KEY `index_transactions_on_account_id` (`account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=115815 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -233,8 +184,9 @@ CREATE TABLE `users` (
   `salt` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -246,110 +198,19 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-02-16  6:03:36
-INSERT INTO schema_migrations (version) VALUES ('20121112001008');
+INSERT INTO `schema_migrations` (version) VALUES
+('20121112001008'),
+('20121112213631'),
+('20121113022247'),
+('20121114062837'),
+('20121120073201'),
+('20121126044735'),
+('20121204072740'),
+('20121204080350'),
+('20130203060617'),
+('20130822041111'),
+('20131117201519'),
+('20150503060156'),
+('20170213202356');
 
-INSERT INTO schema_migrations (version) VALUES ('20121112213631');
-
-INSERT INTO schema_migrations (version) VALUES ('20121113022247');
-
-INSERT INTO schema_migrations (version) VALUES ('20121114062837');
-
-INSERT INTO schema_migrations (version) VALUES ('20121120073201');
-
-INSERT INTO schema_migrations (version) VALUES ('20121126044735');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204072740');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204080350');
-
-INSERT INTO schema_migrations (version) VALUES ('20130203060617');
-
-INSERT INTO schema_migrations (version) VALUES ('20130822041111');
-
-INSERT INTO schema_migrations (version) VALUES ('20131117201519');
-
-INSERT INTO schema_migrations (version) VALUES ('20150503060156');
-
-INSERT INTO schema_migrations (version) VALUES ('20170213202356');
-
-INSERT INTO schema_migrations (version) VALUES ('20121112001008');
-
-INSERT INTO schema_migrations (version) VALUES ('20121112001008');
-
-INSERT INTO schema_migrations (version) VALUES ('20121112213631');
-
-INSERT INTO schema_migrations (version) VALUES ('20121113022247');
-
-INSERT INTO schema_migrations (version) VALUES ('20121114062837');
-
-INSERT INTO schema_migrations (version) VALUES ('20121120073201');
-
-INSERT INTO schema_migrations (version) VALUES ('20121126044735');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204072740');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204080350');
-
-INSERT INTO schema_migrations (version) VALUES ('20130203060617');
-
-INSERT INTO schema_migrations (version) VALUES ('20130822041111');
-
-INSERT INTO schema_migrations (version) VALUES ('20131117201519');
-
-INSERT INTO schema_migrations (version) VALUES ('20150503060156');
-
-INSERT INTO schema_migrations (version) VALUES ('20170213202356');
-
-INSERT INTO schema_migrations (version) VALUES ('20121112001008');
-
-INSERT INTO schema_migrations (version) VALUES ('20121112213631');
-
-INSERT INTO schema_migrations (version) VALUES ('20121113022247');
-
-INSERT INTO schema_migrations (version) VALUES ('20121114062837');
-
-INSERT INTO schema_migrations (version) VALUES ('20121120073201');
-
-INSERT INTO schema_migrations (version) VALUES ('20121126044735');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204072740');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204080350');
-
-INSERT INTO schema_migrations (version) VALUES ('20130203060617');
-
-INSERT INTO schema_migrations (version) VALUES ('20130822041111');
-
-INSERT INTO schema_migrations (version) VALUES ('20131117201519');
-
-INSERT INTO schema_migrations (version) VALUES ('20150503060156');
-
-INSERT INTO schema_migrations (version) VALUES ('20170213202356');
-
-INSERT INTO schema_migrations (version) VALUES ('20121112001008');
-
-INSERT INTO schema_migrations (version) VALUES ('20121112213631');
-
-INSERT INTO schema_migrations (version) VALUES ('20121113022247');
-
-INSERT INTO schema_migrations (version) VALUES ('20121114062837');
-
-INSERT INTO schema_migrations (version) VALUES ('20121120073201');
-
-INSERT INTO schema_migrations (version) VALUES ('20121126044735');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204072740');
-
-INSERT INTO schema_migrations (version) VALUES ('20121204080350');
-
-INSERT INTO schema_migrations (version) VALUES ('20130203060617');
-
-INSERT INTO schema_migrations (version) VALUES ('20130822041111');
-
-INSERT INTO schema_migrations (version) VALUES ('20131117201519');
-
-INSERT INTO schema_migrations (version) VALUES ('20150503060156');
-
-INSERT INTO schema_migrations (version) VALUES ('20170213202356');
 
