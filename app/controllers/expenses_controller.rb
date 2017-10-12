0feb044c421bc5e593481ceacb0055a078c1123c
geordie <geordie.a.henderson@gmail.com>
2017-10-11 22:12:33 -0700

    Make expenses view sortable by amount and move shared code to concerns

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,5 @@
 class ExpensesController < ApplicationController
-
-	helper_method :sort_column, :sort_direction
+	include TransactionControllerConcern
 
 	def index
 		@user = current_user
@@ -44,14 +43,6 @@ class ExpensesController < ApplicationController
 
 	def expense_params
 		params.require(:expense).permit(:name, :description, :account_type, :year)
-	end
-
-	def sort_column
-		['tx_date','account','details','notes','amount'].include?(params[:sort]) ? params[:sort] : "tx_date"
-	end
-
-	def sort_direction
-		%w[asc desc].include?(params[:direction]) ?  params[:direction] : "desc"
 	end
 
 end
