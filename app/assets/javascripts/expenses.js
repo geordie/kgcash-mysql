9dbecd75ed22b975275cd0b802b591c85ad14484
geordie <geordie.a.henderson@gmail.com>
2018-04-02 00:28:17 -0700

    Enable keyboard navigation on expenses

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -5,6 +5,17 @@ $(function (){
 	});
 
 	$('.expense_update').change( function() {
-  		$(this).parents('form:first').submit();	
+  		$(this).parents('form:first').submit();
+	});
+
+	$('.expense_note').keydown( function(e) {
+		if( e.key == " " && e.originalEvent.path[0].localName == 'span'){
+			e.preventDefault();
+			$(this).click();
+		}
+		else if (e.keyCode == 9 && e.originalEvent.path[0].localName == 'input'){
+			var tabIndex = $(this).context.tabIndex
+			$('[tabindex=' + tabIndex + ']').focus();
+		}
 	});
 });
