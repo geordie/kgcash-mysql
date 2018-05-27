97cd2b99d29ad6d962adb2af75a7cfabaca30683
geordie <geordie.a.henderson@gmail.com>
2018-05-26 22:00:33 -0700

    Ensure email address uniqueness for new sign-ups

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	#attr_accessible :username, :email, :password, :password_confirmation
 	#:role
 
+	validates :email, uniqueness: true
 	validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
 	validates_confirmation_of :password, :message => "should match confirmation", :if => :password
 
