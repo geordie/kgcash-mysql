25f4ef0799d7e2d0f1f4aad3d1ae3121edc7044e
geordie <geordie.a.henderson@gmail.com>
2018-09-30 22:16:36 -0700

    Add account paramater to expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,12 +6,14 @@ class ExpensesController < ApplicationController
 
 		@year = params.has_key?(:year) ? params[:year].to_i : Date.today.year
 		category = params.has_key?(:category) ? params[:category].to_i : nil
+		account = params.has_key?(:account) ? params[:account].to_i : nil
 
 		@transactions = @user.transactions
 			.select("id, tx_date, credit, debit, tx_type, details, notes, acct_id_cr, acct_id_dr")
 			.is_expense()
 			.where("(acct_id_dr IS NULL or acct_id_dr in (select id from accounts where account_type = 'Expense'))")
 			.in_debit_acct( category )
+			.in_credit_acct( account )
 			.in_year(@year)
 			.paginate(:page => params[:page])
 			.order(sort_column + ' ' + sort_direction)
