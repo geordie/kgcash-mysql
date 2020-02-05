2810c0b7e62abc46e3b804451f65de9878640fe0
geordie <geordie.a.henderson@gmail.com>
2020-02-04 23:13:07 -0800

    Add total column to income statement table and format as

--- app/class/category_summary.rb
+++ app/class/category_summary.rb
@@ -4,17 +4,18 @@ class CategorySummary
 	def initialize( array, valueKey )
 		
 		@summaryBuilder = Hash.new()
+		catTotal = 0
 
 		for item in array do
 
 			if !@summaryBuilder.key?(item.acct_id)
 				@summaryBuilder[item.acct_id] = Array.new(13){0}
-				@summaryBuilder[item.acct_id][0] = [item.name, item.acct_id]
+				@summaryBuilder[item.acct_id][0] = [item.name, item.acct_id, 0]
 			end
-
-			@summaryBuilder[item.acct_id][item.quantum] = item[valueKey]
+			amountMonth = item[valueKey].nil? ? 0 : item[valueKey]
+			@summaryBuilder[item.acct_id][item.quantum] = amountMonth
+			@summaryBuilder[item.acct_id][0][2] += amountMonth
 		end
-
 		@values = @summaryBuilder.values
 	end
 
