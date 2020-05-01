802536dda0baa653248cee8d0e0cbb97e91355c9
geordie <geordie.a.henderson@gmail.com>
2020-04-30 22:45:20 -0700

    Move income queries out of reports controler into transation model

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,13 +10,7 @@ class ReportsController < ApplicationController
 			@account = nil
 		end
 
-		@transactions_income = @user.transactions
-			.joins( "LEFT JOIN accounts ON accounts.id = transactions.acct_id_cr")
-			.select("sum(credit) as credit, sum(debit) as debit, accounts.name, acct_id_cr")
-			.where("(acct_id_dr in (select id from accounts where account_type = 'Asset' or account_type = 'Expense'))")
-			.where("(acct_id_cr IS NULL or acct_id_cr in (select id from accounts where account_type = 'Income'))")
-			.in_year(@year)
-			.group("acct_id_cr")
+		@transactions_income = Transaction.income_by_category_OLD( @user, @year )
 
 		@transactions_expenses = @user.transactions
 			.joins( "LEFT JOIN accounts ON accounts.id = transactions.acct_id_dr")
@@ -78,43 +72,21 @@ class ReportsController < ApplicationController
 		@quantum = params.has_key?(:quantum) ? params[:quantum] : "month"
 
 		sTimeAggregate = @quantum + "(tx_date)"
-
-		sJoinsIncomeA = "LEFT JOIN accounts as accts_cr ON accts_cr.id = transactions.acct_id_cr"
-		sJoinsIncomeB = "LEFT JOIN accounts as accts_dr ON accts_dr.id = transactions.acct_id_dr"
-
+	
 		sJoinsExpenseA = "LEFT JOIN accounts as accts_cr ON accts_cr.id = transactions.acct_id_cr"
 		sJoinsExpenseB = "LEFT JOIN accounts as accts_dr ON accts_dr.id = transactions.acct_id_dr"
-
-		sSelectIncome = sTimeAggregate + " as quantum, "\
-		"SUM(IF(accts_cr.account_type = 'Income', credit, debit*-1)) as 'income', "\
-		"IF(accts_cr.account_type = 'Income', accts_cr.id, accts_dr.id) as acct_id, "\
-		"IF(accts_cr.account_type = 'Income', accts_cr.name, accts_dr.name) as name"
 
 		sSelectExpense = sTimeAggregate + " as quantum, "\
 		"SUM(IF(accts_dr.account_type = 'Expense', debit, credit*-1)) as 'expenses', "\
 		"IF(accts_dr.account_type = 'Expense', accts_dr.id, accts_cr.id) as acct_id, "\
 		"IF(accts_dr.account_type = 'Expense', accts_dr.name, accts_cr.name) as name"
-
-		sGroupByIncome = sTimeAggregate + ", IF(accts_cr.account_type = 'Income', accts_cr.id, accts_dr.id),"\
-		"IF(accts_cr.account_type = 'Income', accts_cr.name, accts_dr.name)"
-		
+	
 		sGroupByExpense = sTimeAggregate + ", IF(accts_dr.account_type = 'Expense', accts_dr.id, accts_cr.id),"\
 		"IF(accts_dr.account_type = 'Expense', accts_dr.name, accts_cr.name)"
 
 		sOrderByExpense = "acct_id_dr, " + sTimeAggregate
 
-		@income = @user.transactions
-			.joins( sJoinsIncomeA )
-			.joins( sJoinsIncomeB )
-			.select( sSelectIncome )
-			.where("(acct_id_dr in (select id from accounts where account_type = 'Asset') "\
-				"AND acct_id_cr in (select id from accounts where account_type = 'Income')) "\
-					"OR "\
-				"(acct_id_cr in (select id from accounts where account_type = 'Asset') "\
-				"AND acct_id_dr in (select id from accounts where account_type = 'Income'))")
-			.in_year(@year)
-			.group( sGroupByIncome )
-			.order( sTimeAggregate )
+		@income = Transaction.income_by_category(@user, @year)
 
 		gon.income = @income
 
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -40,6 +40,47 @@ class Transaction < ActiveRecord::Base
 	scope :is_asset, lambda{ where("acct_id_cr in (select id from accounts where account_type = 'Asset')") }
 	scope :is_payment, lambda{ where("acct_id_dr in (select id from accounts where account_type = 'Liability')") }
 
+	def self.income_by_category_OLD( user, year )
+		return user.transactions
+			.joins( "LEFT JOIN accounts ON accounts.id = transactions.acct_id_cr")
+			.select("sum(credit) as credit, sum(debit) as debit, accounts.name, acct_id_cr")
+			.where("(acct_id_dr in (select id from accounts where account_type = 'Asset' or account_type = 'Expense'))")
+			.where("(acct_id_cr IS NULL or acct_id_cr in (select id from accounts where account_type = 'Income'))")
+			.in_year(year)
+			.group("acct_id_cr")
+	end
+
+	def self.income_by_category( user, year )
+		sTimeAggregate = "month(tx_date)"
+
+		sJoinsIncomeA = "LEFT JOIN accounts as accts_cr ON accts_cr.id = transactions.acct_id_cr"
+		sJoinsIncomeB = "LEFT JOIN accounts as accts_dr ON accts_dr.id = transactions.acct_id_dr"
+
+		sSelectIncome = sTimeAggregate + " as quantum, "\
+		"SUM(IF(accts_cr.account_type = 'Income', credit, debit*-1)) as 'income', "\
+		"IF(accts_cr.account_type = 'Income', accts_cr.id, accts_dr.id) as acct_id, "\
+		"IF(accts_cr.account_type = 'Income', accts_cr.name, accts_dr.name) as name"
+
+		sGroupByIncome = sTimeAggregate + ", IF(accts_cr.account_type = 'Income', accts_cr.id, accts_dr.id),"\
+		"IF(accts_cr.account_type = 'Income', accts_cr.name, accts_dr.name)"
+
+		return user.transactions
+			.joins( sJoinsIncomeA )
+			.joins( sJoinsIncomeB )
+			.select( sSelectIncome )
+			.where("(acct_id_dr in (select id from accounts where account_type = 'Asset') "\
+				"AND acct_id_cr in (select id from accounts where account_type = 'Income')) "\
+					"OR "\
+				"(acct_id_cr in (select id from accounts where account_type = 'Asset') "\
+				"AND acct_id_dr in (select id from accounts where account_type = 'Income'))")
+			.in_year(year)
+			.group( sGroupByIncome )
+			.order( sTimeAggregate )
+	end
+
+	def self.expenses_by_category(user,year)
+	end
+
 	def ensure_hash
 		if self.tx_hash.to_s == ''
 			self.tx_hash = build_hash
