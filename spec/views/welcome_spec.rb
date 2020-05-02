00fa1226f52b19c381efe5316253984b8a1c4e26
geordie <geordie.a.henderson@gmail.com>
2020-05-02 16:50:46 -0700

    Add an rspec test for the welcome view

---
+++ spec/views/welcome_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe "welcome/index.html.erb", type: :view do
+  it "displays the welcome page" do
+    assign( :year, 2020 )
+    assign( :transactions_expenses, [{:count => 5, :sum => 323}] )
+    assign( :transactions_revenues, [{:count => 6, :sum => 24376}] )
+    
+    render
+
+    expect(rendered).to match(/uncategorized revenue transactions/)
+    expect(rendered).to match(/24376/)
+  end
+end
