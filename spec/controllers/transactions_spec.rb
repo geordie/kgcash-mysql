9f5bff3f8f4c7a4ce6dabbc856be52cab3310d97
geordie <geordie.a.henderson@gmail.com>
2020-05-08 22:53:34 -0700

    Add transactions controller tests

---
+++ spec/controllers/transactions_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+
+RSpec.describe TransactionsController, :type => :controller do
+	include Sorcery::TestHelpers::Rails::Integration
+  include Sorcery::TestHelpers::Rails::Controller
+	#let!(:user) { Fabricate(:user) }
+
+	before :each do
+		@user = Fabricate(:user)
+		login_user
+	end
+
+	describe 'GET #index' do
+
+		it 'responds with 200' do
+			get :index
+			expect(response.status).to eq(200)
+		end
+
+		it "loads @transactions" do
+			t1 = Transaction.create!(
+				:tx_date => DateTime.now, 
+				:user_id => @user.id,
+				:debit => 1.2
+			)
+
+			t2 = Transaction.create!(
+				:tx_date => DateTime.now,
+				:user_id => @user.id,
+				:debit => 3.4
+			)
+
+			get :index
+
+			expect(assigns(:transactions).count).to eq(2)
+			expect(response.status).to eq(200)
+		end
+	end
+
+	describe 'GET #show' do
+	  let(:token) { double :accessible? => true }
+
+      it 'responds with 200' do
+        get :index
+        expect(response.status).to eq(200)
+      end
+    end
+end
