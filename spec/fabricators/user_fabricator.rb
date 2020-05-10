12ee10c685a9974e5183dba7c59863e9f3a33189
geordie <geordie.a.henderson@gmail.com>
2020-05-10 16:52:46 -0700

    Add user account fabrication to user fabricator

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,9 +1,23 @@
 Fabricator(:user, :class_name => "User") do
-  accounts(count: 3)
-  id { sequence }
-  username { "admin" }
-  password { "admin" }
-  email { "whatever@whatever.com" }
-  salt { "asdasdastr4325234324sdfds" }
-  crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt("secret", "asdasdastr4325234324sdfds") }
+	id { sequence }
+	accounts(count: 9){
+		|attrs, i|
+			case i
+			when 1..2
+				Fabricate(:asset)
+			when 3..4
+				Fabricate(:liability)
+			when 5..6
+				Fabricate(:income)
+			when 7..9
+				Fabricate(:expense)
+			else
+				Fabricate(:expense)
+			end
+	}
+	username { "admin" }
+	password { "admin" }
+	email { "whatever@whatever.com" }
+	salt { "asdasdastr4325234324sdfds" }
+	crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt("secret", "asdasdastr4325234324sdfds") }
 end
