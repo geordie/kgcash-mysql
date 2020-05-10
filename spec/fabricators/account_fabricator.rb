97fc976714db8af1464de232f1014af43cd78c56
geordie <geordie.a.henderson@gmail.com>
2020-05-10 16:51:36 -0700

    Add account subclass fabricators for four main account types

--- spec/fabricators/account_fabricator.rb
+++ spec/fabricators/account_fabricator.rb
@@ -3,3 +3,20 @@ Fabricator(:account, :class_name => "Account") do
 	name { Faker::Name.first_name }
 	account_type { %w(Asset Expense Income Liability).sample }
 end
+
+Fabricator(:asset, from: :account) do
+	account_type { "Asset"}
+end
+
+Fabricator(:expense, from: :account) do
+	account_type { "Expense"}
+end
+
+Fabricator(:income, from: :account) do
+	account_type { "Income"}
+end
+
+Fabricator(:liability, from: :account) do
+	account_type { "Liability"}
+end
+
