6d45c0632e4befafb89789bbc4aae6518e4a3e1d
geordie <geordie.a.henderson@gmail.com>
2020-05-23 12:21:53 -0700

    Added get_tx_amount to spec_helper and changes spaces to tabs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,53 +9,58 @@ require 'rspec/rails'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+	# ## Mock Framework
+	#
+	# If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+	#
+	# config.mock_with :mocha
+	# config.mock_with :flexmock
+	# config.mock_with :rr
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+	# Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+	config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+	# If you're not using ActiveRecord, or you'd prefer not to run each of your
+	# examples within a transaction, remove the following line or assign false
+	# instead of true.
+	config.use_transactional_fixtures = true
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+	# If true, the base class of anonymous controllers will be inferred
+	# automatically. This will be the default behavior in future versions of
+	# rspec-rails.
+	config.infer_base_class_for_anonymous_controllers = false
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
+	# Run specs in random order to surface order dependencies. If you find an
+	# order dependency and want to debug it, you can fix the order by providing
+	# the seed, which is printed after each run.
+	#     --seed 1234
+	config.order = "random"
 
-  config.include Sorcery::TestHelpers::Rails
+	config.include Sorcery::TestHelpers::Rails
 
-  config.include Capybara::DSL
+	config.include Capybara::DSL
 end
 
 module Sorcery
-  module TestHelpers
-    module Rails
+	module TestHelpers
+		module Rails
 
-      def user_login(user, password)
-        page.driver.post(user_sessions_url, { username: user, password: password})
-      end
+			def user_login(user, password)
+				page.driver.post(user_sessions_url, { username: user, password: password})
+			end
 
-      def login_user(user, password)
-        visit login_path
+			def login_user(user, password)
+				visit login_path
 
-        fill_in 'email', with: user.email
-        fill_in 'password', with: 'admin'
-        click_button 'login'
-      end
-    end
-  end
+				fill_in 'email', with: user.email
+				fill_in 'password', with: 'admin'
+				click_button 'login'
+			end
+
+			def get_tx_amount()
+				return rand(100) + (rand(100)/100)
+			end
+
+		end
+	end
 end
