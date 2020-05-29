f625166d48e2324d7fbabeb01fdfe7e824c12227
geordie <geordie.a.henderson@gmail.com>
2020-05-28 22:50:12 -0700

    Remove unused application fabricator

--- spec/fabricators/application_fabricator.rb
+++
@@ -1,8 +0,0 @@
-Fabricator(:application, :class_name => "Doorkeeper::Application") do
-  id { sequence }
-  name { "admin" }
-  uid { "sdfsdfsdf"	 }
-  secret { "whatever" }
-  redirect_uri { "http://localhost:3000" }
-  owner_id { 1 }
-end
