04052bd35b85e8f77cb30d6d7e17b50cdf532108
geordie <geordie.a.henderson@gmail.com>
2020-05-28 23:07:59 -0700

    Add tests for date_math lib

---
+++ spec/lib/datemath_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'date_math'
+
+describe "DateMath" do
+	it "gets last month correctly from first month" do
+		expect( DateMath.last_month(1,2020) ).to eq([12,2019])
+	end
+
+	it "gets last month correctly from final month" do
+		expect( DateMath.last_month(12,2020) ).to eq([11,2020])
+	end
+
+	it "gets next month correctly from final month" do
+		expect( DateMath.next_month(12,2020) ).to eq([1,2021])
+	end
+
+	it "gets next month correctly from first month" do
+		expect( DateMath.next_month(1,2020) ).to eq([2,2020])
+	end
+
+	it "gets last week correctly from first week" do
+		expect( DateMath.last_week(1,2020) ).to eq([52,2019])
+	end
+
+	it "gets last week correctly from final week" do
+		expect( DateMath.last_week(52,2020) ).to eq([51,2020])
+	end
+
+	it "gets next week correctly from final week" do
+		expect( DateMath.next_week(53,2020) ).to eq([1,2021])
+	end
+
+	it "gets next week correctly from first week" do
+		expect( DateMath.next_week(1,2020) ).to eq([2,2020])
+	end
+end
