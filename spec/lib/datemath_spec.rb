e788c43976ecfc073b7638bf4ac594a6671b38ac
geordie <geordie.a.henderson@gmail.com>
2020-06-10 22:57:14 -0700

    Add tests for date_math.days_past_in_year method

--- spec/lib/datemath_spec.rb
+++ spec/lib/datemath_spec.rb
@@ -33,4 +33,12 @@ describe "DateMath" do
 	it "gets next week correctly from first week" do
 		expect( DateMath.next_week(1,2020) ).to eq([2,2020])
 	end
+
+	it "gets the days past in the specified year" do
+		today = DateTime.now()
+		expect( DateMath.days_past_in_year(today.year)).to eq(today.yday)
+		expect( DateMath.days_past_in_year(2019)).to eq(365)
+		expect( DateMath.days_past_in_year(2016)).to eq(366)
+		expect( DateMath.days_past_in_year(3000)).to eq(0)
+	end
 end
