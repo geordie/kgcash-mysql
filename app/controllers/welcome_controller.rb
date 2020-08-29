7ccb8cff2297116e67b5fa2210fbae5de6472d8d
geordie <geordie.a.henderson@gmail.com>
2020-08-29 14:02:39 -0700

    Update welcome controller to use model functions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,21 +3,11 @@ class WelcomeController < ApplicationController
 	def index
 
 		@user = current_user
-
-		@month = params.has_key?(:month) ? params[:month].to_i : nil
 		@year = params.has_key?(:year) ? params[:year].to_i : Date.today.year
 
-		@transactions_expenses = @user.transactions
-			.select("count(*) as count, sum(credit) as sum")
-			.is_expense()
-			.where("(acct_id_dr IS NULL)")
-			.in_year(@year)
+		@transactions_expenses = Transaction.uncategorized_expenses(@user, @year)
 
-		@transactions_revenues = @user.transactions
-			.select("count(*) as count, sum(debit) as sum")
-			.is_liability()
-			.where("(acct_id_cr IS NULL)")
-			.in_year(@year)
+		@transactions_revenues = Transaction.uncategorized_revenue(@user, @year)
 
 		respond_to do |format|
 			format.html #index.html.erb
