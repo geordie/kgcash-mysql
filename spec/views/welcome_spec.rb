11ca77d6d638eef5bbb28e23316a3da96145136a
geordie <geordie.a.henderson@gmail.com>
2020-08-29 23:17:33 -0700

    Fix broken test

--- spec/views/welcome_spec.rb
+++ spec/views/welcome_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe "welcome/index.html.erb", type: :view do
   it "displays the welcome page" do
     assign( :year, 2020 )
-    assign( :transactions_expenses, [{:count => 5, :sum => 323}] )
-    assign( :transactions_revenues, [{:count => 6, :sum => 24376}] )
+    assign( :transactions_expenses, [{:count => 5, :uncategorized_expenses => 323}] )
+    assign( :transactions_revenues, [{:count => 6, :uncategorized_revenue => 24376}] )
     
     render
 
