1bd32e181e9b375a2436f9985a9d6c8b2cb8ac59
geordie <geordie.a.henderson@gmail.com>
2020-09-19 22:44:57 -0700

    Improve variable names in welcome controller and views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,9 +5,10 @@ class WelcomeController < ApplicationController
 		@user = current_user
 		@year = params.has_key?(:year) ? params[:year].to_i : Date.today.year
 
-		@transactions_expenses = Transaction.uncategorized_expenses(@user, @year)
+		@expenses_uncategorized = Transaction.uncategorized_expenses(@user, @year)
+		@revenues_uncategorized = Transaction.uncategorized_revenue(@user, @year)
 
-		@transactions_revenues = Transaction.uncategorized_revenue(@user, @year)
+		# expenses_all_time
 
 		respond_to do |format|
 			format.html #index.html.erb
