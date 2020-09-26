8a02552c4df950024a91923d4bee801ea395be92
geordie <geordie.a.henderson@gmail.com>
2020-09-26 00:26:13 -0700

    Add net income to welcome index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
 		@user = current_user
 		@year = params.has_key?(:year) ? params[:year].to_i : Date.today.year
 
+		# Get expenses
 		expenses = Transaction.expenses_all_time(@user,@year)
 		@expenses = 0
 
@@ -13,6 +14,7 @@ class WelcomeController < ApplicationController
 				0 : expenses[0][:expenses]
 		end
 
+		# Get revenues
 		revenues = Transaction.revenues_all_time(@user,@year)
 		@revenue = 0
 
@@ -21,8 +23,32 @@ class WelcomeController < ApplicationController
 				0 : revenues[0][:revenue]
 		end
 
-		@expenses_uncategorized = Transaction.uncategorized_expenses(@user, @year)
-		@revenues_uncategorized = Transaction.uncategorized_revenue(@user, @year)
+		# Get uncategorized expenses
+		expenses_uncategorized = Transaction.uncategorized_expenses(@user, @year)
+
+		@expenses_uncategorized_amount = 0
+		@expenses_uncategorized_count = 0
+		if !expenses_uncategorized.nil? && expenses_uncategorized.length > 0
+			@expenses_uncategorized_amount =
+				expenses_uncategorized[0][:uncategorized_expenses]
+			@expenses_uncategorized_count =
+				expenses_uncategorized[0][:count]
+		end
+
+		# Get uncategorized revenue
+		revenues_uncategorized = Transaction.uncategorized_revenue(@user, @year)
+
+		@revenues_uncategorized_amount = 0
+		@revenues_uncategorized_count = 0
+		if !revenues_uncategorized.nil? && revenues_uncategorized.length > 0
+			@revenues_uncategorized_amount =
+				revenues_uncategorized[0][:uncategorized_revenue]
+			@revenues_uncategorized_count =
+				revenues_uncategorized[0][:count]
+		end
+
+		@net_income = (@revenue + @revenues_uncategorized_amount) -
+			(@expenses + @expenses_uncategorized_amount)
 
 		respond_to do |format|
 			format.html #index.html.erb
--- spec/views/welcome_spec.rb
+++ spec/views/welcome_spec.rb
@@ -3,8 +3,10 @@ require "spec_helper"
 describe "welcome/index.html.erb", type: :view do
   it "displays the welcome page" do
     assign( :year, 2020 )
-    assign( :expenses_uncategorized, [{:count => 5, :uncategorized_expenses => 323}] )
-    assign( :revenues_uncategorized, [{:count => 6, :uncategorized_revenue => 24376}] )
+    assign( :expenses_uncategorized_amount, 323 )
+    assign( :expenses_uncategorized_count, 5 )
+    assign( :revenues_uncategorized_amount, 24376 )
+    assign( :revenues_uncategorized_count, 6 )
     
     render
 
