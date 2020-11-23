2824a64e748a24a69aa51b9e6aa1f7c61b551caa
geordie <geordie.a.henderson@gmail.com>
2020-11-22 17:00:09 -0800

    Remove more js code

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -4,8 +4,4 @@ $(function (){
   		$(this).parents('form:first').submit();
 	});
 
-	$('.transaction_filter').change( function() {
-		var selectedValue = $(this)[0].value;
-  		$(this).parents('form:first').submit();
-	});
 });
