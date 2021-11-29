c5adbaf15809cfb3eb336c02dc80ee0e071d723e
geordie <geordie.a.henderson@gmail.com>
2021-11-28 22:06:36 -0800

    Add migration to update transactions created_at / updated_at columns to not be nullable (and set good values for existing nulls)

---
+++ db/migrate/20211128214519_set_transactions_created_at_not_null.rb
@@ -0,0 +1,16 @@
+class SetTransactionsCreatedAtNotNull < ActiveRecord::Migration[4.2]
+  def change
+    Transaction.where(created_at: nil).each do |tx|
+      tx.created_at = tx.tx_date
+    	tx.save();
+    end
+
+    Transaction.where(updated_at: nil).each do |tx|
+      tx.updated_at = Date.now
+    	tx.save();
+    end
+
+    change_column_null :transactions, :created_at, false
+    change_column_null :transactions, :updated_at, false
+  end
+end
