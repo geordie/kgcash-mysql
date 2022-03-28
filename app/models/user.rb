4ae64333e944b56a8c909c395b0a89f2b124b702
geordie <geordie.a.henderson@gmail.com>
2022-03-27 21:30:01 -0700

    Update user model to use new uniqueness validator format for updated version of rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	#attr_accessible :username, :email, :password, :password_confirmation
 	#:role
 
-	validates :email, uniqueness: true
+	validates_uniqueness_of :email, case_sensitive: false
 	validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
 	validates_confirmation_of :password, :message => "should match confirmation", :if => :password
 
