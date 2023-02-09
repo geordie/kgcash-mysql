5ee98c9611ae994dd5efeb3159c839dae2d6768d
geordie <geordie.a.henderson@gmail.com>
2023-02-09 15:29:02 -0800

    Fix bug with in-place keyboard editing of transations

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -1,12 +1,12 @@
 $(function (){
 
 	$('.expense_note').keydown( function(e) {
-		if( e.key == " " && e.originalEvent.path[0].localName == 'span'){
+		if( e.key == " " && e.currentTarget.localName == 'span'){
 			e.preventDefault();
 			$(this).click();
 		}
-		else if (e.keyCode == 9 && e.originalEvent.path[0].localName == 'input'){
-			var tabIndex = $(this).context.tabIndex
+		else if (e.keyCode == 9 && e.currentTarget.localName == 'input'){
+			var tabIndex = $(this).context.tabIndex;
 			$('[tabindex=' + tabIndex + ']').focus();
 		}
 	});
