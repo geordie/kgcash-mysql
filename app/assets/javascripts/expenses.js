c26f52ad7659a093522b6c9469467dfe9301a46c
geordie <geordie.a.henderson@gmail.com>
2023-02-15 22:21:12 -0800

    Fix bug that prevented adding spaces to expense comments during in place editing

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -2,7 +2,6 @@ $(function (){
 
 	$('.expense_note').keydown( function(e) {
 		if( e.key == " " && e.currentTarget.localName == 'span'){
-			e.preventDefault();
 			$(this).click();
 		}
 		else if (e.keyCode == 9 && e.currentTarget.localName == 'input'){
