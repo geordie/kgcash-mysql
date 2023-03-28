99af29730b593c4a4219b8edfba33388d69dc8c9
geordie <geordie.a.henderson@gmail.com>
2023-03-27 21:49:33 -0700

    Migrate incomes controller and views to pagy from will_paginate

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -10,7 +10,7 @@ class IncomesController < ApplicationController
 
 		sJoinsIncome = "LEFT JOIN accounts as accts_cr ON accts_cr.id = transactions.acct_id_cr"
 
-		@transactions = @user.transactions
+		@pagy, @transactions = pagy(@user.transactions
 			.joins(sJoinsIncome)
 			.select("transactions.id, tx_date, credit, debit, debit as 'amount', tx_type, details, notes, acct_id_cr, acct_id_dr, parent_id, " \
 			"IF(accts_cr.account_type = 'Income', 'credit', 'debit') as txType "\
@@ -22,8 +22,7 @@ class IncomesController < ApplicationController
 			"AND acct_id_dr in (select id from accounts where account_type = 'Income'))")
 			.in_account( category )
 			.in_month_year(@month, @year)
-			.paginate(:page => params[:page])
-			.order(sort_column + ' ' + sort_direction)
+			.order(sort_column + ' ' + sort_direction))
 
 		respond_to do |format|
 			format.html #index.html.erb
@@ -36,13 +35,12 @@ class IncomesController < ApplicationController
 
 		@year = params.has_key?(:year) ? params[:year].to_i : Date.today.year
 
-		@transactions = @user.transactions
+		@pagy, @transactions = pagy(@user.transactions
 			.select("id, tx_date, credit, debit, debit as 'amount', tx_type, details, notes, acct_id_cr, acct_id_dr, parent_id, 'credit' as txType")
 			.is_liability()
 			.where("(acct_id_cr IS NULL)")
 			.in_year(@year)
-			.paginate(:page => params[:page])
-			.order(sort_column + ' ' + sort_direction)
+			.order(sort_column + ' ' + sort_direction))
 
 		respond_to do |format|
 			format.html #index.html.erb
