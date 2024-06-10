f122a491b8156eea1afa651fa65044ef30b2500f
geordie <geordie.a.henderson@gmail.com>
2024-06-10 12:42:03 -0700

    Remove budget dependencies on budget_categories

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,7 +1,5 @@
 class Budget < ActiveRecord::Base
   belongs_to :user
-  has_many :budget_categories, :dependent => :delete_all
-  has_many :accounts, :through => :budget_categories
 
   def sortedAccounts
   	return self.accounts.sort{|a,b| a.name.downcase <=> b.name.downcase }
