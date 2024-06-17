4e5cdf5c166bf789801e2d434b71c5826d4c357a
geordie <geordie.a.henderson@gmail.com>
2024-06-16 20:06:11 -0700

    Remove commented code from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,6 @@ users = User.create([
     {username: 'example', email: 'geordie.a.henderson@gmail.com', crypted_password: "$2a$10$Ub1mJwax0nZpN5JU9ED5eu4EE.ad1.95K1dXi.J8DAdLBHPDn6oi6", salt: 'VTKmPVJFsaivMbFxVv5F'}
   ])
 
-#transactions = Transaction.create([
-#  {tx_hash:'abc',   tx_date:DateTime.now, debit:100.23, category_id:categories[0], user_id:users[0]},
-#  {tx_hash:'defg', tx_date:DateTime.now, debit: 56.78, category_id:categories[1], user_id:users[0]},
-#  {tx_hash:'hij',  tx_date:DateTime.now, debit: 18.68, category_id:categories[2], user_id:users[0]}
-#  ])
-
 puts 'DONE SEED'
 
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
