5c51188f3e016c87f7e4b278fa70891434a93abb
geordie <geordie.a.henderson@gmail.com>
2024-06-19 11:26:02 -0700

    Add a utility javascript function to get default echarts multibar chart options

---
+++ app/assets/javascripts/echarts.js
@@ -0,0 +1,49 @@
+function getEchartsMultibarOptions( multiBarData ) {
+  // Configure the multi-bar chart options
+  var multiBarOptions = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      },
+      formatter: function(params) {
+        var tooltipText = params[0].axisValueLabel + '<br/>';
+        params.forEach(function(param) {
+          tooltipText += param.marker + ' ' + param.seriesName + ': $' + param.value + '<br/>';
+        });
+        return tooltipText;
+      }
+    },
+    legend: {
+      data: multiBarData.map(function (item) {
+        return item.name;
+      }),
+      selected: {
+        'Credits': false,
+        'Debits': false
+      }
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+      type: 'value',
+      axisLabel: {
+        formatter: function(value) {
+          return '$' + value;
+        }
+      }
+    },
+    series: multiBarData.map(function (item) {
+      return {
+        name: item.name,
+        type: 'bar',
+        data: item.values
+      };
+    })
+  };
+
+  // Set the multi-bar chart options
+  return multiBarOptions;
+}
