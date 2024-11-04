c51ef7bbe5ab57fbc9665f61029c5dd90c045417
geordie <geordie.a.henderson@gmail.com>
2024-11-04 10:57:03 -0800

    Add notes routes

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ end
 
 resources :categories
 
+resources :notes
 
 match 'login' => 'user_sessions#new', :as => :login, via: [:get, :post]
 match 'logout' => 'user_sessions#destroy', :as => :logout, via: [:get, :post]
