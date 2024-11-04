1f3d86a6834f395367bb8190cacbcf7cca4e464a
geordie <geordie.a.henderson@gmail.com>
2024-11-04 14:21:02 -0800

    Add a test that for failing login attempts

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -13,5 +13,12 @@ describe "Authentication", :type => :request do
 
     expect(page).to have_content("Login successful")
   end
-# other examples ...
+
+  it "does not log in the user with invalid credentials" do
+    user = Fabricate(:user)
+    post user_sessions_path, params: { user_session: { email: user.email, password: 'wrongpassword' } }
+    expect(response).to render_template(:new)
+    expect(response.body).to include("Login failed.")
+  end
+
 end
