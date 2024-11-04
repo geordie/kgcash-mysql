20d3fb2fea4824827ff46805d2f57d0bed9ba725
geordie <geordie.a.henderson@gmail.com>
2024-11-04 10:42:56 -0800

    Remove budgets association from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ActiveRecord::Base
 	validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
 	validates_confirmation_of :password, :message => "should match confirmation", :if => :password
 
-	has_many :budgets
 	has_many :categories
 	has_many :transactions
 	has_and_belongs_to_many :documents
