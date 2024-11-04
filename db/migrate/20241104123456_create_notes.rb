1406e5477d17b12b2f6fbce464318c2a1ec69827
geordie <geordie.a.henderson@gmail.com>
2024-11-04 12:30:31 -0800

    Update creates notes table migration to reference user so that all notes are owned by one user

--- db/migrate/20241104123456_create_notes.rb
+++ db/migrate/20241104123456_create_notes.rb
@@ -4,6 +4,7 @@ class CreateNotes < ActiveRecord::Migration[6.1]
       t.text :note_text, null: false
       t.decimal :amount, null: false, precision: 10, scale: 2
       t.references :transaction, null: true
+      t.references :user, null: false, foreign_key: true, type: :integer
 
       t.timestamps
     end
