602c6a2584487a0714f4ef723dbe9a4034391f57
geordie <geordie.a.henderson@gmail.com>
2025-10-17 22:01:58 -0700

    Add disk service for rails test context

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,4 +44,7 @@ Kgcash::Application.configure do
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
+
+  # Configure Active Storage for tests
+  config.active_storage.service = :test
 end
